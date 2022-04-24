130dcb45c32fe6afcc06bba880c74dbe4e05b6f9
Lenar Sagidullin <lenar_sagidullin@epam.com>
2022-04-24 10:31:14 +0300

    Updates in OOP homework (1)

--- src/main/java/com/epam/rd/at/java_oop/Room.java
+++ src/main/java/com/epam/rd/at/java_oop/Room.java
@@ -6,28 +6,28 @@ import java.util.Arrays;
 
 public class Room  {
 
-   private SomeAppliance[] roomWithAppliance;
+   private SomeAppliance[] manyAppliances;
 
     public Room() {
-        this.roomWithAppliance = new SomeAppliance[0];
+        this.manyAppliances = new SomeAppliance[0];
     }
 
     public void addAppliance(SomeAppliance appliance) {
-        SomeAppliance[] result = new SomeAppliance[this.roomWithAppliance.length + 1];
-        for ( int i =0; i < this.roomWithAppliance.length; i++) {
-            result[i] = this.roomWithAppliance[i];
+        SomeAppliance[] result = new SomeAppliance[this.manyAppliances.length + 1];
+        for (int i = 0; i < this.manyAppliances.length; i++) {
+            result[i] = this.manyAppliances[i];
         }
-        result[this.roomWithAppliance.length] = appliance;
-        this.roomWithAppliance = result;
+        result[this.manyAppliances.length] = appliance;
+        this.manyAppliances = result;
     }
 
     public void  sortByPrice() {
-        Arrays.sort(this.roomWithAppliance, new PriceComparator());
+        Arrays.sort(this.manyAppliances, new PriceComparator());
     }
 
     public int calculateTotalPowerConsumption(){
         int result = 0;
-        for (SomeAppliance appliance: roomWithAppliance) {
+        for (SomeAppliance appliance: manyAppliances) {
             if (appliance.getIsPluggedIn()){
                 result += appliance.getPowerConsumptionInWT();
             }
@@ -36,7 +36,7 @@ public class Room  {
     }
 
     public SomeAppliance findApplianceByPower(int minPower, int maxPower) {
-        for (SomeAppliance appliance: roomWithAppliance) {
+        for (SomeAppliance appliance: manyAppliances) {
             if (appliance.isDeviceInPowerConsumptionRange(minPower, maxPower)) {
                 return appliance;
             }
