b61a8d60f98181010441581bddb3d4873f47b9cd
Lenar Sagidullin <lenar_sagidullin@epam.com>
2022-06-01 20:46:44 +0300

    Updating StreamExercises

--- Stream API/src/main/java/com/epam/rd/at/java_stream_api/exercises/StreamExercises.java
+++ Stream API/src/main/java/com/epam/rd/at/java_stream_api/exercises/StreamExercises.java
@@ -8,6 +8,7 @@ import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static java.util.concurrent.TimeUnit.DAYS;
 
@@ -15,23 +16,18 @@ public class StreamExercises {
 
     // task 1
     public static List<Integer> generateNumbers(int x) {
-        List<Integer> result = IntStream.rangeClosed(1, x).boxed().collect(Collectors.toList());
-        return result;
+        return IntStream.rangeClosed(1, x).boxed().collect(Collectors.toList());
     }
 
     // task 2
     public static LocalDate findMostDistantDate(LocalDate anchorDate, LocalDate... datesToCompare) {
-        List<LocalDate> resultsList = new ArrayList<>();
-        return Arrays.stream(datesToCompare).max(LocalDate::compareTo).get();
-    }
-
-    public static long testy (LocalDate dateAnch, LocalDate date) {
-        return Math.abs(ChronoUnit.DAYS.between(dateAnch, date));
+        return Arrays.stream(datesToCompare)
+                .max(Comparator.comparingLong(date -> Math.abs(ChronoUnit.DAYS.between(date,anchorDate)))).orElse(null);
     }
 
     // task 3
     public static <T> List<T> getOddElements(List<T> list) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return IntStream.range(0,list.size()).filter(x -> x % 2 != 0).mapToObj(x -> list.get(x)).collect(Collectors.toList());
     }
 
     // task 4
@@ -41,17 +37,30 @@ public class StreamExercises {
 
     // task 5
     public static List<String> sortByLengthDesc(List<String> list) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        LengthComparator comparator = new LengthComparator();
+        return list.stream().sorted(comparator).collect(Collectors.toList());
     }
 
     // task 6
     public static boolean checkNoNegativeValues(List<Integer> list) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return list.stream().allMatch(s -> s >= 0);
     }
 
     // task 7
     public static List<LocalDate> getRandomUniqueDates(int year, int limit) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return Stream.generate(() -> createRandomDate(year, year))
+                .limit(limit-1)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+    public static LocalDate createRandomDate(int startYear, int endYear) {
+        int day = createRandomIntBetween(1, 28);
+        int month = createRandomIntBetween(1, 12);
+        int year = createRandomIntBetween(startYear, endYear);
+        return LocalDate.of(year, month, day);
+    }
+    public static int createRandomIntBetween(int start, int end) {
+        return start + (int) Math.round(Math.random() * (end - start));
     }
 
     // task 8
